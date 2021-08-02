cbcf1424bcbf941ef6f13f36c92ee3753737a793
Daniel L. Miranda <danieldelacerdamiranda@protonmail.com>
2021-08-02 11:39:44 -0300

    fix/adds additional verifications

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,49 @@
 git submodule init
 git submodule update
 
+# files paths
 ZSHRC=$HOME/.zshrc
 ZSH_ALIASES=$HOME/.zsh_aliases
+OMZ_DIR=$HOME/.oh-my-zsh
 GITCONFIG=$HOME/.gitconfig
 WSLCONFIG=/mnt/c/Users/daniellmiranda
+WINDOWS_TERMINAL=/mnt/c/Users/daniellmiranda/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+
+# packages to be installed
+packages=('git' 'zsh' 'paru' 'stow')
+
+for i in $packages; do
+  if ! pacman -Qi $i > /dev/null 2>&1; then
+    sudo pacman -S $i > /dev/null 2>&1
+  fi
+done
 
 # zsh
+<<<<<<< Updated upstream
 test -f ZSHRC && rm ZSHRC
 test -f ZSH_ALIASES && rm ZSH_ALIASES
 stow -t "$HOME" zsh
 source ~/.zshrc
+=======
+rm -f $ZSHRC
+rm -f $ZSH_ALIASES
+stow -t ~ zsh
+>>>>>>> Stashed changes
 
 # git
-test -f GITCONFIG && rm GITCONFIG 
+rm -f $GITCONFIG 
 stow -t "$HOME" git
 
 # wsl2
-test -f WSLCONFIG && rm WSLCONFIG
 cp -f ./wsl/.wslconfig /mnt/c/Users/daniellmiranda
 
 # windows terminal
-cp ./windows-terminal/settings.json /mnt/c/Users/daniellmiranda/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+cp ./windows-terminal/settings.json $WINDOWS_TERMINAL
+
+# installs oh my zsh
+if ! [ -d "$OMZ_DIR" ] > /dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# installs/updates zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
