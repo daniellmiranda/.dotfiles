218f0a59dae8c3a75f5aa3b77dcf56fd669678db
Daniel L. Miranda <danieldelacerdamiranda@protonmail.com>
2021-09-18 00:48:04 -0300

    fix/packages installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# pulls and updates submodules
-git submodule init
-git submodule update
+# script path
+script_path=${pwd}
 
 # files paths
 ZSHRC=$HOME/.zshrc
@@ -13,11 +12,18 @@ WSLCONFIG=/mnt/c/Users/daniellmiranda
 WINDOWS_TERMINAL=/mnt/c/Users/daniellmiranda/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
 
 # packages to be installed (in official Arch repos)
-packages=('git' 'zsh' 'base-devel' 'stow' 'bat')
+packages=('git' 'zsh' 'stow' 'bat' 'reflector' 'neofetch')
+packages_groups=('base-devel')
 
-for i in $packages; do
+for i in ${packages[@]}; do
   if ! pacman -Qi $i > /dev/null 2>&1; then
-    sudo pacman -S $i > /dev/null 2>&1
+    sudo pacman -S $i --noconfirm
+  fi
+done
+
+for i in ${packages_groups[@]}; do
+  if ! pacman -Qg $i > /dev/null 2>&1; then
+    sudo pacman -S $i --noconfirm
   fi
 done
 
@@ -26,30 +32,28 @@ if ! pacman -Qi aura-bin > /dev/null 2>&1; then
   cd $HOME
   git clone https://aur.archlinux.org/aura-bin.git
   cd aura-bin
-  makepkg -si
-  rm -r aura-bin
+  makepkg -si --noconfirm
+  cd $HOME
+  sudo rm -r aura-bin
+  cd $script_path
 fi
-
 
 # installs Pazi from AUR
 if ! pacman -Qi pazi > /dev/null 2>&1; then
-  sudo aura -A pazi
+  sudo aura -A pazi --noconfirm
 fi
-
-# install/update nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # zsh
 rm -f $ZSHRC
 rm -f $ZSH_ALIASES
-stow -t ~ zsh
+stow -t $HOME zsh
 
 # git
 rm -f $GITCONFIG 
-stow -t "$HOME" git
+stow -t $HOME git
 
-# wsl2
-cp -f ./wsl/.wslconfig /mnt/c/Users/daniellmiranda
+# wsl2 - comment to disable ram limit
+# cp -f ./wsl/.wslconfig /mnt/c/Users/daniellmiranda
 
 # windows terminal
 cp ./windows-terminal/settings.json $WINDOWS_TERMINAL
@@ -71,3 +75,6 @@ zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions" >> $ZSHRC
 fi
+
+# install/update nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
